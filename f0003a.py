560694079d0e05811e9cd2085f18dd29b25ef3cd
kocsisjonatan <54977355+kocsisjonatan@users.noreply.github.com>
2020-10-15 13:13:03 +0200

    Add files via upload

---
+++ f0003a.py
@@ -0,0 +1,4 @@
+kereszt_név = input('mi a kereszt neved ?')
+vezeték_név = input('mi a vezeték neved ?')
+
+print("A te neved " + vezeték_név + kereszt_név + ' !')
---
+++ f0003b.py
@@ -0,0 +1,7 @@
+születési_év = (input("mikkor születél"))
+
+kereszt_név = input('mi a kereszt neved ?')
+vezeték_név = input('mi a vezeték neved ?')
+
+print("A te neved " + vezeték_név + kereszt_név +  '!')
+print("És ekkor születél " + születési_év)
